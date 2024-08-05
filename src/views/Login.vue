975056ed2548a59eeb0a9f3d636eba7e3bccbffb
Gabriel Abalde <gabiabaldee@gmail.com>
2024-08-05 12:34:34 -0300

    Primer acercamiento al Navbar

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="text-5xl text-blue">Login xdddddd</h1>
+  <h1 class="text-5xl text-blue">Login xdddddd</h1>
+  <h1 class="text-5xl text-blue">Login xdddddd</h1>
+  <h1 class="text-5xl text-blue">Login xdddddd</h1>
+  <h1 class="text-5xl text-blue">Login xdddddd</h1>
+  <h1 class="text-5xl text-blue">Login xdddddd</h1>
+  <h1 class="text-5xl text-blue">Login xdddddd</h1>
+  <h1 class="text-5xl text-blue">Login xdddddd</h1>
+  <h1 class="text-5xl text-blue">Login xdddddd</h1>
+  <h1 class="text-5xl text-blue">Login xdddddd</h1>
+  <h1 class="text-5xl text-blue">Login xdddddd</h1>
+  <h1 class="text-5xl text-blue">Login xdddddd</h1>
+</template>
